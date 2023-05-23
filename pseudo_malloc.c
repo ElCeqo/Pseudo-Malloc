c2cb24c4ca725597ad53a016ffa1b12bbfebfb54
ElCeqo <fr.corvaglia@gmail.com>
2023-05-23 22:28:22 +0200

    some definitions

--- pseudo_malloc.c
+++ pseudo_malloc.c
@@ -1,1 +1,22 @@
-//Implementation
+#include "pseudo_malloc.h"
+#include "buddy_allocator.h"
+
+void * pseudo_malloc(size_t size){
+    //TODO
+
+    /*from man:
+        If size is 0 malloc returns NULL
+        On error returns NULL*/
+}
+
+void pseudo_free(void * ptr){
+    //TODO
+
+    /*from man:
+        frees memory space pointed by *ptr
+        which must have been returned by a previous
+        malloc().
+        if free(ptr) has already been called -> undefined behaviour
+        if ptr is NULL no operation occurs
+        on error returns NULL*/
+}
--- pseudo_malloc.h
+++ pseudo_malloc.h
@@ -1,1 +1,13 @@
-//.h file 
+#pragma once
+#include <stdlib.h>
+
+//page size in bytes (checked with getconf PAGE_SIZE)
+#define PAGE_SIZE 4096
+
+// functions to implement
+
+// alloc bytes bytes
+void * pseudo_malloc(size_t size);
+
+// free
+void pseudo_free(void * ptr);
